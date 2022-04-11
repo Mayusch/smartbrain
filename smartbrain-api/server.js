a4d1f8c4429be3dd3ae6ace43bfa8a20977c814f
Leanne Afergan <leanneafergan1@Leannes-MBP.lan>
2022-04-11 21:36:53 +0300

    first commit

---
+++ smartbrain-api/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'leanneafergan1',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send('success') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfileGet(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', (req, res) => { image.handleCall(req, res) })
+// app.post('/imageurl', image.handleCall())
+
+app.listen(3001, () => {
+    console.log('app is running on port 3001');
+})
+
